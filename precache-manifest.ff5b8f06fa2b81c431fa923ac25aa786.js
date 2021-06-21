4e1d61e825b8175d80b506d0a3e2ee95a9b05038
Nakul <nakulgopal13199@gmail.com>
2021-06-21 17:19:44 +0530

    Updates

---
+++ precache-manifest.ff5b8f06fa2b81c431fa923ac25aa786.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c6172625a298f67a23e39bd8a4a36186",
+    "url": "/NewsApp/index.html"
+  },
+  {
+    "revision": "9ce2f732451bf422774d",
+    "url": "/NewsApp/static/css/main.9c59207b.chunk.css"
+  },
+  {
+    "revision": "2829f658d05344f4543e",
+    "url": "/NewsApp/static/js/2.4904b4fd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/NewsApp/static/js/2.4904b4fd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9ce2f732451bf422774d",
+    "url": "/NewsApp/static/js/main.ce643116.chunk.js"
+  },
+  {
+    "revision": "fa6acf9707c670447a19",
+    "url": "/NewsApp/static/js/runtime-main.3c23a02d.js"
+  }
+]);
